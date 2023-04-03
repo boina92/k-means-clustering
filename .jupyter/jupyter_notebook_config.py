3161a79306aa2018d54e1ab29909e65fc00d5c1d
Boina <boina.babu@gmail.com>
2023-04-03 23:56:41 +0200

    update config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -5,7 +5,6 @@ c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.allow_credentials = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.notebook_dir = os.getcwd()
-c.NotebookApp.password = 'sha1:2c5b5574939c:7e4edf4c4ec3f75c3c7e26c3d1f8f1adad8029ba'
 c.NotebookApp.password_required = False
 c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.tornado_settings = {
