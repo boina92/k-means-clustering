014ef77ad2b3a0de90c13a93b864193e87f084b3
Boina <boina.babu@gmail.com>
2023-04-03 23:52:00 +0200

    update config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -5,6 +5,7 @@ c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.allow_credentials = True
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.notebook_dir = os.getcwd()
+c.NotebookApp.password = 'sha1:2c5b5574939c:7e4edf4c4ec3f75c3c7e26c3d1f8f1adad8029ba'
 c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.tornado_settings = {
     'headers': {
