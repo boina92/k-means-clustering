9ff94e0e8552882896318b396229836cabcebd2d
Boina <boina.babu@gmail.com>
2023-04-04 00:34:31 +0200

    update config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -2,13 +2,10 @@ import os
 
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.disable_check_xsrf = True
-c.NotebookApp.allow_credentials = True
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.notebook_dir = os.getcwd()
+c.NotebookApp.ip = '*'
 c.NotebookApp.token = ''
-c.NotebookApp.password = u''
+c.NotebookApp.password = ''
 c.NotebookApp.password_required = False
-c.NotebookApp.disable_check_xsrf = True
 c.NotebookApp.tornado_settings = {
     'headers': {
         'Content-Security-Policy': "frame-ancestors 'self' *"
