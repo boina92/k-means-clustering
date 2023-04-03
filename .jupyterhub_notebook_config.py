2244b1c2fdbcb0d7766cb2c843c17c37767f1016
Boina <boina.babu@gmail.com>
2023-04-03 18:16:52 +0200

    add config file

--- .jupyterhub_notebook_config.py
+++ .jupyterhub_notebook_config.py
@@ -1,2 +1,2 @@
 c.NotebookApp.tornado_settings = {'headers': {
-    'Content-Security-Policy': "frame-ancestors 'self' http://localhost:3000"}}
+    'Content-Security-Policy': "frame-ancestors * 'self' "}}
